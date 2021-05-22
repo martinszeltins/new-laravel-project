3150c20062b40e6074ffc030305cd7717949de40
Martins Zeltins <martins.zeltins@arkbauer.com>
2021-05-22 21:41:22 +0300

    Added tailwind component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,30 @@
     </head>
 
     <body>
-        <h1>Hello Laravel!</h1>
+        <div class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+            <img class="h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto md:mx-0"
+                 src="{{ asset('img/digimon.jpg') }}"
+            />
+
+            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <blockquote>
+                    <p class="text-lg font-semibold">
+                        “Tailwind CSS is the only framework that I've seen scale
+                        on large teams. It’s easy to customize, adapts to any design,
+                        and the build size is tiny.”
+                    </p>
+                </blockquote>
+
+                <figcaption class="font-medium">
+                    <div class="text-cyan-600">
+                        Sarah Dayan
+                    </div>
+
+                    <div class="text-gray-500">
+                        Staff Engineer, Algolia
+                    </div>
+                </figcaption>
+            </div>
+        </div>
     </body>
 </html>
